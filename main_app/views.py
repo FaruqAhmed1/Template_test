43cec5e7cd653ebde03343e91195f5e9ecae1a34
FaruqAhmed1 <faruqahmedroni@gmail.com>
2020-02-27 19:54:04 +0600

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import TemplateView
+
+class Homeview(TemplateView):
+    template_name = "index.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['name']  = "Faruq"
+        context ['country'] = "Bangladesh"
+        list = [1,2,3,4,5]
+        context['list'] = list
+    
+        return context 
+class Aboutview(TemplateView):
+    template_name = "about.html"
+           
